1b15ab849ee1a43552c87549cd14233a01dd96b2
Player <player1038727@gmail.com>
2022-03-03 01:27:59 +0300

    a

--- x/views.py
+++ x/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-#import requests
+import requests
 
 
 #from .models import Greeting
@@ -8,7 +8,17 @@ from django.http import HttpResponse
 # Create your views here.
 def x(request):
     # return HttpResponse('Hello from Python!')
-    context = '1'
+    m = 'm'
+    u = 'u'
+    r = requests.post("https://discord.com/api/webhooks/922904892031524864/7HK6lO9sAJBm4unBnI8CaTmewdGxml3Y4bgj1xhOAsAygW4DFrcHZQsvInZfvlRvwR8y", data={'content': m, 'username': u, 'avatar_url': r'https://assets.stickpng.com/images/580b57fcd9996e24bc43c540.png'})
+
+    if r.status_code == '204':
+        context = {'responce': '@'+u+', спасибо, записал!'}
+    elif m == '':
+        context = {'responce': '@'+u+', ошибка: нужно ввести текст, который произнес стример (в одном сообщении, после команды)'}
+    else:
+        context = {'responce': 'Error'}
+
     return render(request, "x.html", context)
 
 
