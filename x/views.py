0cfd4ac1e2f7be2f2afc7a3730544af80a408d7d
Player <player1038727@gmail.com>
2022-03-03 01:29:55 +0300

    a

--- x/views.py
+++ x/views.py
@@ -12,7 +12,7 @@ def x(request):
     u = 'u'
     r = requests.post("https://discord.com/api/webhooks/922904892031524864/7HK6lO9sAJBm4unBnI8CaTmewdGxml3Y4bgj1xhOAsAygW4DFrcHZQsvInZfvlRvwR8y", data={'content': m, 'username': u, 'avatar_url': r'https://assets.stickpng.com/images/580b57fcd9996e24bc43c540.png'})
 
-    if r.status_code == '204':
+    if r.status_code == 204:
         context = {'responce': '@'+u+', спасибо, записал!'}
     elif m == '':
         context = {'responce': '@'+u+', ошибка: нужно ввести текст, который произнес стример (в одном сообщении, после команды)'}
@@ -20,5 +20,3 @@ def x(request):
         context = {'responce': 'Error'}
 
     return render(request, "x.html", context)
-
-
